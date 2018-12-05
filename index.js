4027a9a947f75e6ea56b212315fd0922feb2dd2b
Paul Riverain <paulriverain@yahoo.com>
2018-12-05 21:36:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//var katzDeliLine = []
+// var otherDeli = ["Steven", "Blake", "Avi"]
+
 function takeANumber(katzDeliLine, name){
  katzDeliLine.push(`${name}`)
 return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
