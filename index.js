0697a9220b0bc3b58fbdf45def183ca79d223bda
Paul Riverain <paulriverain@yahoo.com>
2018-12-09 16:09:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
+
 function takeANumber(katzDeliLine, name){
- katzDeliLine.push(`${name}`)
-return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+  katzDeliLine.push(`${name}`)
+  return `Welcome, ${katzDeliLine}. You are number ${katzDeliLine.length[i]} in line.`
 }
 
-function nowServing (katzDeliLine){
-let i = 0;
-while (i < katzDeliLine.length){
-  i++;
-}
-  if (katzDeliLine.length === 0){
-    return ("There is nobody waiting to be served!")
-  } else{
-return (`Currently serving ${katzDeliLine.shift()}.`)
-  }
-}
-
-function currentLine(katzDeliLine){
-  let line = [];
-  for (let i = 0; i < katzDeliLine.length; i++){
-    line.push(" "+ [i + 1]+ ". "+ katzDeliLine[i])
-  }
-  if (katzDeliLine.length === 0){
-    return ("The line is currently empty.")
-  }else{
-    return (`The line is currently:${line}`)
-  }
-}
+//let line = [];
